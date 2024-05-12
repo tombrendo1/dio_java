8067ec81d29ac5bc538b378a5f2f77ab1a714bde
tombrendo1 <brendliveira2@gmail.com>
2024-05-12 16:41:50 -0300

    teste commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,6 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+       boolean nova_conta = false;
+       
+    }
+}
---
+++ ContaBanco/src/usuario.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class usuario {
+    public static void main(String[] args) {
+
+        ContaBanco conta_banco  = new ContaBanco();
+        System.out.println(conta_banco.nova_conta);
+        //Scanner sc = new Scanner(System.in);
+        
+        //int A, B, PROD;
+        
+        //A = sc.nextInt();
+        //B = sc.nextInt();
+        
+        //PROD = A * B;
+
+        //System.out.println("PROD = " + PROD);
+    }
+}
